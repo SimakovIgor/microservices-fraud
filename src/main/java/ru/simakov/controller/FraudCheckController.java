4f5f376e91c8ce6bdd4bbde3f0ee9a01742f4a6a
Igor Simakov <i.simakov@banki.ru>
2023-06-04 15:33:02 +0800

    Enable Distributed Tracing using Micrometer, OpenTelemetry, And Jaeger

--- src/main/java/ru/simakov/controller/FraudCheckController.java
+++ src/main/java/ru/simakov/controller/FraudCheckController.java
@@ -1,10 +1,10 @@
 package ru.simakov.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import ru.simakov.clients.fraud.FraudCheckResponse;
 import ru.simakov.service.FraudCheckService;
@@ -12,11 +12,13 @@ import ru.simakov.service.FraudCheckService;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @RequiredArgsConstructor
+@Slf4j
 public class FraudCheckController {
     private final FraudCheckService fraudCheckService;
 
     @GetMapping
     public FraudCheckResponse isFraudster(@RequestHeader Long customerId) {
+        log.info("New check isFraudster for customerId {}", customerId);
         return fraudCheckService.isFraudulentCustomer(customerId);
     }
 }
